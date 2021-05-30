93d5b850965a74ca62044adde477bf3f6106361e
Matthew Davis <matthew@matthewdavis.io>
2021-05-30 16:22:46 -0400

    bump

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,17 @@
 import * as core from '@actions/core';
 
 async function run(): Promise<void> {
-
     try {
-
         const name: string = core.getInput('kubernetes_token');
         const kubernetes_endpoint: string = core.getInput('kubernetes_token');
         const kubernetes_token: string = core.getInput('kubernetes_token');
 
-        core.debug(`name = ${ name }`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+        core.debug(`name = ${name} ${kubernetes_token} ${kubernetes_endpoint}`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
         core.setOutput('time', new Date().toTimeString());
-
     } catch (error) {
-
         core.setFailed(error.message);
-
     }
-
 }
 
 run();
