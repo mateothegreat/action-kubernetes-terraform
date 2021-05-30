c970c6e42bfe2450c71f374a8f0ffd187195a569
Matthew Davis <matthew@matthewdavis.io>
2021-05-30 16:13:44 -0400

    bump

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    try {
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+        const name: string = core.getInput('kubernetes_token');
+        const kubernetes_endpoint: string = core.getInput('kubernetes_token');
+        const kubernetes_token: string = core.getInput('kubernetes_token');
+
+        core.debug(`name = ${ name }`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+
+        core.setOutput('time', new Date().toTimeString());
+
+    } catch (error) {
+
+        core.setFailed(error.message);
+
+    }
+
 }
 
-run()
+run();
